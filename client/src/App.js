8a684f70d27561273c3b2871a956698ddaffd926
Aleksandr Shepelev <aleksandr@teramind.co>
2020-12-04 17:55:04 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,284 @@
+import React, {Component} from 'react';
+import {w3cwebsocket as W3CWebSocket} from "websocket";
+import Identicon from 'react-identicons';
+import {
+    Navbar,
+    NavbarBrand,
+    UncontrolledTooltip
+} from 'reactstrap';
+import 'medium-editor/dist/css/medium-editor.css';
+import 'medium-editor/dist/css/themes/default.css';
+import './App.css';
+
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
+
+const typesDef = {
+    CREATE_CONVERSATION_EVENT: "createConversationEvent",
+    JOIN_CONVERSATION_EVENT: "joinConversationEvent",
+    BAN_DECK_EVENT: "banDeckEvent",
+    USER_DISCONNECTED_EVENT: "userDisconnectedEvent",
+    UPDATE_LOG_EVENT: "logUpdateEvent",
+    ERROR_EVENT: "errorEvent",
+};
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUsers: [],
+            log: [],
+            userId: '',
+            username: '',
+            conversationId: '',
+            bannedDeck: '',
+            error: '',
+            banSubmitted: false,
+        };
+    }
+
+    handleUsernameChange = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    };
+
+    back = () => {
+        this.setState({
+            conversationId: ''
+        });
+    };
+
+    submitBan = () => {
+        const userId = this.state.userId;
+        const conversationId = this.state.conversationId;
+        const bannedDeck = this.bannedDeckInput.value;
+        const data = {
+            userId,
+            conversationId,
+            bannedDeck,
+        };
+        if (bannedDeck.trim()) {
+            this.setState({
+                ...data
+            }, () => {
+                client.send(JSON.stringify({
+                    ...data,
+                    type: typesDef.BAN_DECK_EVENT,
+                }));
+            });
+        }
+    };
+
+    createConversation = () => {
+        const username = this.usernameInput.value;
+        if (username.trim()) {
+            const data = {
+                username,
+            };
+            this.setState({
+                ...data
+            }, () => {
+                client.send(JSON.stringify({
+                    ...data,
+                    type: typesDef.CREATE_CONVERSATION_EVENT
+                }));
+            });
+        }
+    };
+
+    joinConversation = () => {
+        const username = this.usernameInput.value;
+        const conversationId = this.conversationIdInput.value;
+        if (username.trim()) {
+            const data = {
+                username,
+                conversationId
+            };
+            this.setState({
+                username
+            }, () => {
+                client.send(JSON.stringify({
+                    ...data,
+                    type: typesDef.JOIN_CONVERSATION_EVENT
+                }));
+            });
+        }
+    };
+
+    componentWillMount() {
+        client.onopen = () => {
+            console.log('WebSocket Client Connected');
+        };
+
+        client.onmessage = (message) => {
+            const dataFromServer = JSON.parse(message.data);
+            const stateToChange = {};
+            switch (dataFromServer.type) {
+                case typesDef.CREATE_CONVERSATION_EVENT:
+                    stateToChange.currentUsers = dataFromServer.data.conversation.users;
+                    stateToChange.log = dataFromServer.data.conversation.log;
+                    stateToChange.conversationId = dataFromServer.data.conversation.conversationId;
+                    stateToChange.userId = dataFromServer.data.userId;
+                    stateToChange.error = '';
+                    break;
+
+                case typesDef.JOIN_CONVERSATION_EVENT:
+                    stateToChange.currentUsers = dataFromServer.data.conversation.users;
+                    stateToChange.log = dataFromServer.data.conversation.log;
+                    stateToChange.conversationId = dataFromServer.data.conversation.conversationId;
+                    stateToChange.userId = dataFromServer.data.userId;
+                    stateToChange.error = '';
+
+                    break;
+
+                case typesDef.BAN_DECK_EVENT:
+                    stateToChange.banSubmitted = true;
+                    stateToChange.bannedDeck = dataFromServer.data.bannedDeck;
+                    stateToChange.error = '';
+
+                    break;
+
+                case typesDef.UPDATE_LOG_EVENT:
+                    stateToChange.log = dataFromServer.data.log;
+                    stateToChange.error = '';
+
+                    break;
+
+                case typesDef.ERROR_EVENT:
+                    stateToChange.error = dataFromServer.data.message;
+                    break;
+
+                default:
+                    break;
+            }
+            this.setState({
+                ...stateToChange
+            });
+        };
+    }
+
+    showLoginSection = () => (
+        <div className="account">
+            <div className="account__wrapper">
+                <div className="account__card">
+                    <div className="account__profile">
+                        <Identicon className="account__avatar" size={64} string="randomness"/>
+                        <p className="account__name">Hello, KeyForge Player!</p>
+                        <p className="account__sub">This is a tool to simultaneously reveal your bans for triad
+                            games</p>
+                        <p className="account__sub">Username:</p>
+                    </div>
+                    <input name="username" ref={(input) => {
+                        this.usernameInput = input;
+                    }}
+                       value={this.state.username}
+                       onChange={this.handleUsernameChange}
+                       className="form-control"/>
+                    <p className="button__desc">If you want to create new conversation, just press this button:</p>
+                    <button type="button" onClick={() => this.createConversation()}
+                            disabled={!this.state.username}
+                            className="btn btn-primary account__btn">Create Conversation
+                    </button>
+                    <br></br>
+                    <p className="button__desc">Or enter conversation ID received from your opponent here:</p>
+                    <input name="conversation-id" ref={(input) => {
+                        this.conversationIdInput = input;
+                    }}
+                           disabled={!this.state.username}
+                           className="form-control"/>
+                    <button type="button" onClick={() => this.joinConversation()}
+                            disabled={!this.state.username}
+                            className="btn btn-primary account__btn">Join Conversation
+                    </button>
+                    {this.state.error && (
+                        <div className="error-msg">
+                            <p className="error-text">{this.state.error}</p>
+                        </div>)}
+                </div>
+
+            </div>
+        </div>
+    )
+
+    showBanSection = () => (
+        <div className="account">
+            <div className="account__wrapper">
+                <div className="account__card">
+                    <div className="account__profile">
+                        <div className="currentusers">
+                            {this.state.currentUsers.map(user => (
+                                <React.Fragment>
+                                  <span id={user.username} className="userInfo" key={user.username}>
+                                    <Identicon className="account__avatar" style={{backgroundColor: user.randomcolor}}
+                                               size={40}
+                                               string={user.username}/>
+                                  </span>
+                                    <UncontrolledTooltip placement="top" target={user.username}>
+                                        {user.username}
+                                    </UncontrolledTooltip>
+                                </React.Fragment>
+                            ))}
+                        </div>
+                        <div>
+                            <React.Fragment>
+                                <p className="conv__id">Conversation ID: {this.state.conversationId}</p>
+                            </React.Fragment>
+                        </div>
+                    </div>
+                    <p className="button__desc">Enter deck name you want to ban:</p>
+                    <input name="banned-deck" ref={(input) => {
+                        this.bannedDeckInput = input;
+                    }}
+                           className="form-control"
+                           disabled={this.state.banSubmitted}
+                    />
+                    <button type="button" onClick={() => this.submitBan()}
+                            disabled={this.state.banSubmitted}
+                            className="btn btn-primary account__btn">Submit your ban
+                    </button>
+                    {this.state.banSubmitted && (
+                        <div className="ok-msg">
+                            <p className="ok-text">Your decision accepted</p>
+                        </div>)}
+                    {this.state.error && (
+                        <div className="error-msg">
+                            <p className="error-text">{this.state.error}</p>
+                        </div>)}
+
+                    <p className="button__desc">Attention! You will not be able to enter same conversation again</p>
+
+                    <button type="button" onClick={() => this.back()}
+                            className="btn btn-primary account__btn">BACK
+                    </button>
+
+                </div>
+
+                <div className="history-holder">
+                    <ul>
+                        {this.state.log.map((activity, index) => <li
+                            key={`activity-${index}`}>{activity}</li>)}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+
+    render() {
+        const {
+            conversationId
+        } = this.state;
+        return (
+            <React.Fragment>
+                <Navbar color="light" light>
+                    <NavbarBrand href="/">Triad ban tool</NavbarBrand>
+                </Navbar>
+                <div className="container-fluid">
+                    {conversationId ? this.showBanSection() : this.showLoginSection()}
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,183 @@
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+const wsServer = new webSocketServer({
+    httpServer: server
+});
+
+// Generates unique ID for every new connection
+const getUniqueID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() + '-' + s4();
+};
+
+// I'm maintaining all active connections in this object
+const clients = {};
+
+const conversations = {};
+
+// I'm maintaining all active users in this object
+const users = {};
+
+const sendMessage = (conversation, json, userId) => {
+    // We are sending the current data to all connected clients
+    console.log(JSON.stringify(JSON.parse(json), null, 2));
+
+    if (!conversation) return;
+
+    if (userId) {
+        clients[userId].sendUTF(json);
+
+    } else {
+        conversation.users.forEach((user) => {
+            clients[user.userId].sendUTF(json);
+        })
+    }
+};
+
+const sendLogUpdate = (conversation) => {
+        if (!conversation) return;
+
+        conversation.users.forEach((user) => {
+            clients[user.userId].sendUTF(JSON.stringify({
+                type: typesDef.UPDATE_LOG_EVENT,
+                data: {
+                    log: conversation.log
+                }
+            }));
+        })
+    };
+
+const sendError = (userId, message) => {
+    clients[userId].sendUTF(
+        (JSON.stringify({
+            type: typesDef.ERROR_EVENT,
+            data: {
+                message
+            }
+        }))
+    )
+};
+
+const sendBans = (conversation) => {
+    if (conversation.users.length < 2) {
+        return;
+    }
+    const user1 = conversation.users[0];
+    const user2 = conversation.users[1];
+    const ban1 = conversation.bans[user1.userId];
+    const ban2 = conversation.bans[user2.userId];
+
+    if (ban1 && ban2) {
+        conversation.log.push(`${user1.username} banned ${ban1}`);
+        conversation.log.push(`${user2.username} banned ${ban2}`);
+        sendLogUpdate(conversation);
+    }
+};
+
+
+const typesDef = {
+    CREATE_CONVERSATION_EVENT: "createConversationEvent",
+    JOIN_CONVERSATION_EVENT: "joinConversationEvent",
+    BAN_DECK_EVENT: "banDeckEvent",
+    USER_DISCONNECTED_EVENT: "userDisconnectedEvent",
+    UPDATE_LOG_EVENT: "logUpdateEvent",
+    ERROR_EVENT: "errorEvent",
+};
+
+wsServer.on('request', function (request) {
+    var userId = getUniqueID();
+    console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+
+    // You can rewrite this part of the code to accept only the requests from allowed origin
+    const connection = request.accept(null, request.origin);
+    clients[userId] = connection;
+    console.log('connected: ' + userId + ' in ' + Object.getOwnPropertyNames(clients));
+    connection.on('message', function (message) {
+        var conversation = null;
+        if (message.type === 'utf8') {
+            const dataFromClient = JSON.parse(message.utf8Data);
+            const json = {type: dataFromClient.type};
+            var conversationId = dataFromClient.conversationId;
+            var user = users[dataFromClient.userId];
+
+            console.log(JSON.stringify(dataFromClient, null, 2));
+
+            switch (dataFromClient.type) {
+                case typesDef.CREATE_CONVERSATION_EVENT:
+                    users[userId] = dataFromClient;
+
+                    user = users[userId];
+                    user.userId = userId;
+
+                    conversationId = getUniqueID();
+                    conversations[conversationId] = {
+                        id: conversationId,
+                        log: [],
+                        users: [],
+                        bans: {},
+                    };
+                    conversation = conversations[conversationId];
+                    conversation.conversationId = conversationId;
+                    conversation.users.push(user);
+                    conversation.log.push(`${dataFromClient.username} joined conversation`);
+                    conversation.log.push(`Conversation ID: ${conversationId}. Send it to your opponent`);
+
+                    json.data = {userId, conversation};
+
+                    sendMessage(conversation, JSON.stringify(json));
+
+                    break;
+                case typesDef.JOIN_CONVERSATION_EVENT:
+                    conversation = conversations[conversationId];
+                    console.log(conversation);
+                    if (!conversation) {
+                        return sendError(userId, "conversation not found");
+                    }
+
+                    if (conversation.users.length > 2) {
+                        return sendError(userId, "two players already in conversation");
+                    }
+
+                    users[userId] = dataFromClient;
+                    user = users[userId];
+                    user.userId = userId;
+                    conversation.users.push(user);
+                    conversation.log.push(`${dataFromClient.username} joined conversation`);
+
+                    json.data = {userId, conversation};
+
+                    sendMessage(conversation, JSON.stringify(json));
+
+                    break;
+                case typesDef.BAN_DECK_EVENT:
+                    var id = dataFromClient.userId;
+                    var bannedDeck = dataFromClient.bannedDeck;
+
+                    user = users[id];
+                    conversation = conversations[conversationId];
+
+                    conversation.bans[id] = bannedDeck;
+                    conversation.log.push(`${user.username} made their choice`);
+
+                    json.data = {
+                        bannedDeck,
+                        conversation
+                    };
+
+                    sendMessage(conversation, JSON.stringify(json), userId);
+                    sendLogUpdate(conversation);
+                    sendBans(conversation);
+                    break;
+            }
+        }
+    });
+    // user disconnected
+    connection.on('close', function (connection) {
+        console.log((new Date()) + " Peer " + userId + " disconnected.");
+        delete clients[userId];
+    });
+});
